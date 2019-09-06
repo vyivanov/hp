6fee542b7200cec93b66c5f06b65c35c6a940824
Vladimir Ivanov <vladimir.ivanov@tra.ai>
2019-09-06 23:16:21 +0300

    Add CV link

--- js/aux-tools.js
+++ js/aux-tools.js
@@ -1,4 +1,9 @@
-function cv_alert()
+function alert()
 {
     alert("I don't looking for a job at the moment")
+}
+
+function timestamp(self)
+{
+    self.href=self.href.split('?')[0]+'?'+new Date().getTime()
 }
